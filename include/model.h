6ae34bfdf90ae94b52425def680d04bf8857bc36
GamerNoTitle <GamerNoTitle@outlook.com>
2024-12-04 21:09:16 +0800

    feat: add new model Question and add the initializer to intialize the program and add the entry

--- include/model.h
+++ include/model.h
@@ -8,7 +8,7 @@ History:        暂无
     1.  Date: 2024/12/2
         Author: 吴沛熹
         ID: GamerNoTitle
-        Modification:   新建了文件，并加入了头部文件注释，说明本头文件的功能
+        Modification:   [+] 新建了文件，并加入了头部文件注释，说明本头文件的功能
                         [+] 添加了新的结构体User，来定义用户，存储用户的各种属性
                         [+] 添加了新的函数isAdmin，用于判断用户是否是管理员（鉴权使用）
     2.  Date: 2024/12/3
@@ -16,9 +16,13 @@ History:        暂无
         ID: GamerNoTitle
         Modification:   [-] 删除了函数isAdmin，并去掉了管理员类型用户
                         [+] 新增了权限管理结构体Permission，用于定义用户的各种权限
-
+    3.  Date: 2024/12/4
+        Author: 吴沛熹
+        ID: GamerNoTitle
+        Modification:   [+] 添加了新的函数getPermission，用于在初始化用户的时候快速获取用户权限
  */
 
+/**************************** 用户模型和权限部分 ****************************/
 
 /**
  * @brief 定义变量类型Permission，作为用户的权限结构
@@ -27,7 +31,7 @@ History:        暂无
 struct Permission
 {
     int stuAnswer;  // 学生权限：答题
-    int stuInspectInfo; // 学生权限：查看个人信息、成绩
+    int stuInspectPersonalInfo; // 学生权限：查看个人信息、成绩
     int stuInspectExamInfo; // 学生权限：查看考试信息
     int teaManageExam; // 教师权限：管理考试（包括新建、修改、删除）
     int teaManageStudent;   // 教师权限：管理学生（包括导入、修改、深处）
@@ -52,5 +56,84 @@ struct User
     unsigned int number; // 因为按照我这一届的学号为312400XXXX，已经十位数字了
                          // 而且学号只有正数，所以在4294967295的范围内能存下
                          // 工号按照辅导员的五位来看，完全足够
-    struct Permission permission;
+    struct Permission permission;   // 使用上面定义的结构体Permission来决定用户的权限
 };
+
+/**
+ * @brief 定义函数getPermission，用于获取用户的权限，返回包含用户权限数据的Permission结构体
+ * 
+ * Parameter:
+ *  @user   User类型的结构体，通过获取User.role来决定用户的权限
+ * 
+ * Returns:
+ *  @permission Permission类型的结构体，里面包含了用户的权限
+ */
+Permission getPermission(struct User user)
+{
+    struct Permission currentPermission;
+    switch (user.role)  // 0为学生，1为老师
+    {
+        case 0: // 学生权限
+            currentPermission.stuAnswer = 1;
+            currentPermission.stuInspectPersonalInfo = 1;
+            currentPermission.stuInspectExamInfo = 1;
+            currentPermission.teaManageExam = 0;
+            currentPermission.teaManageStudent = 0;
+            currentPermission.teaInspectStudentInfo = 0;
+            currentPermission.teaInspectExamScores = 0;
+            currentPermission.generalEditInfo = 1;
+            break;
+        case 1: // 老师权限
+            currentPermission.stuAnswer = 0;
+            currentPermission.stuInspectPersonalInfo = 0;
+            currentPermission.stuInspectExamInfo = 0;
+            currentPermission.teaManageExam = 1;
+            currentPermission.teaManageStudent = 1;
+            currentPermission.teaInspectStudentInfo = 1;
+            currentPermission.teaInspectExamScores = 1;
+            currentPermission.generalEditInfo = 1;
+            break;
+        default:    // 未知用户类型，为了安全起见，除了更改个人信息的权限，其他权限一律不给
+            currentPermission.stuAnswer = 0;
+            currentPermission.stuInspectPersonalInfo = 0;
+            currentPermission.stuInspectExamInfo = 0;
+            currentPermission.teaManageExam = 0;
+            currentPermission.teaManageStudent = 0;
+            currentPermission.teaInspectStudentInfo = 0;
+            currentPermission.teaInspectExamScores = 0;
+            currentPermission.generalEditInfo = 1;
+            break;
+    }
+    return currentPermission;
+}
+
+/**************************** 用户模型和权限部分结束 ****************************/
+
+/**************************** 问题模型部分 ****************************/
+/**
+ * @brief 定义结构体Question，用于定义考试中的每个问题的节点
+ * 
+ * @details 因为考虑可以做到随机题目出现顺序，所以用链表来存储整个问题的列表
+ *          所以在这里使用Question结构体，来作为问题的节点，便于后期进行随机化
+ * 
+ */
+struct Question{
+    /**
+     * @brief 定义结构体QuestionData，用于存储问题
+     * 
+     * @details QuestionData包括 num1 (int), op (char), num2 (int)
+     *          其中 num1 (int) 表示这个算式中的第一个数字
+     *          op (char) 表示这个算式中的运算符，仅限 + - * /
+     *          num2 (int) 是这个算式中的第二个数字
+     * 
+     */
+    struct QuestionData
+    {
+        int num1;   // 第一个操作数
+        char op;    // 运算符
+        int num2;   // 第二个操作数
+    };
+    struct QuestionData data;   // 问题的数据部分，类型为上面定义的 QuestionData
+    struct Question *nextQuestion;  // 问题链的下一个问题
+};
+/**************************** 问题模型部分结束 ****************************/
---
+++ utils/initializer.c
@@ -0,0 +1,215 @@
+/*
+Copyright © GamerNoTitle 2024. All rights reserved.
+File name: initializer.c
+Author: 吴沛熹      ID: GamerNoTitle    Version: v1.0   Date:
+Description:    本文件用于初始化整个程序，当这个程序第一次运行的时候，运行本初始化程序
+Others:         暂无
+History:        暂无
+    1.  Date: 2024/12/4
+        Author: 吴沛熹
+        ID: GamerNoTitle
+        Modification: [+] 新建了文件，并加入了头部文件注释，说明本头文件的功能
+                      [+] 新建了函数initialize()，用于进行程序的初始化
+                      [+] 新建了函数initalize_database()用来进行数据库的初始化
+                      [+] 添加了将日志保存到logs文件夹的特定文件的功能
+ */
+
+#include "../lib/sqlite3.h"
+#include <io.h>
+#include <direct.h>
+#include <stdio.h>
+#include <errno.h>
+#include <windows.h>
+
+#include "../include/utils.h"  // 引入自己写的头文件utils.h，来调用里面已经写好的一些trick函数
+
+#define ACCESS_EXIST_MODE 0 // 访问模式，0为是否存在，在_access函数中使用
+#define ACCESS_RW_MODE 6    // 访问模式，6代表是否有读写权限（读取为2，写入为4），在_access函数中使用
+
+#define DB_FOLDER "db"
+#define EXAMINATION_DB "db/examination.db"
+#define SCORES_DB "db/scores.db"
+#define USER_DB "db/user.db"
+#define LOG_FOLDER "logs" // 日志文件夹路径
+#define LOG_FILE "logs/initialization.log" // 日志文件路径
+
+#define LOGLEVEL_INFO "INFO"
+#define LOGLEVEL_ERROR "ERROR"
+
+/**
+ * @brief 数据库初始化函数，用于初始化不同的数据库
+ * 
+ * @param db_filename 数据库文件路径
+ * @param command   数据库初始化命令，只运行一次，如果有多条命令需要调用多次这个函数
+ * @param log_file  日志文件，用于保存初始化日志情况
+ */
+void initialize_database(const char *db_filename, const char *command, FILE *log_file)
+{
+    // 定义变量存储时间
+    char current_time[20];
+
+    // 定义数据库指针变量
+    sqlite3 *db;
+    char *err_msg = 0;
+
+    // 打开数据库
+    int rc = sqlite3_open(db_filename, &db);
+    if (rc)
+    {
+        get_current_time(current_time, sizeof(current_time));
+        fprintf(log_file, "%s [%s]: 打开数据库 '%s' 失败：%s\n", current_time, LOGLEVEL_ERROR, db_filename, sqlite3_errmsg(db));
+        sqlite3_close(db);
+        return;
+    }
+
+    // 创建表
+    rc = sqlite3_exec(db, command, 0, 0, &err_msg);
+    if (rc != SQLITE_OK)
+    {
+        get_current_time(current_time, sizeof(current_time));
+        fprintf(log_file, "%s [%s]: 创建表失败：%s\n", current_time, LOGLEVEL_ERROR, err_msg);
+        sqlite3_free(err_msg);  // 运行失败后释放占用的内存
+    }
+    else
+    {
+        get_current_time(current_time, sizeof(current_time));
+        fprintf(log_file, "%s [%s]: 对数据库 '%s' 初始化命令 %s 运行成功\n", current_time, LOGLEVEL_INFO, db_filename, command);
+    }
+
+    sqlite3_close(db); // 关闭数据库连接
+}
+
+
+/**
+ * @brief 初始化函数，当程序运行时，执行初始化操作，且将日志保存到文件中
+ */
+void initialize()
+{
+    // 检查是否存在 logs 文件夹
+    if (_access(LOG_FOLDER, ACCESS_EXIST_MODE) != 0) 
+    {
+        if (_mkdir(LOG_FOLDER) != 0)
+        {
+            printf("无法创建 logs 文件夹：错误码 %d，%s\n", errno, strerror(errno));
+            return;  // 如果无法创建日志文件夹，返回
+        }
+    }
+
+    // 打开日志文件，如果不存在则创建
+    FILE *log_file = fopen(LOG_FILE, "a"); // 'a' 表示附加模式
+    if (log_file == NULL)
+    {
+        printf("无法打开日志文件：%s\n", strerror(errno));
+        return;
+    }
+
+    // 定义时间变量
+    char current_time[20];
+
+    // 尝试创建 db 文件夹
+    if (_access(DB_FOLDER, ACCESS_EXIST_MODE) != 0) // 当 db 文件夹不存在时
+    {
+        if (_mkdir(DB_FOLDER) == 0) // 创建文件夹
+        {
+            get_current_time(current_time, sizeof(current_time));
+            fprintf(log_file, "%s [%s]: 文件夹 '%s' 创建成功。\n", current_time, LOGLEVEL_INFO, DB_FOLDER); // 创建成功的时候，写入日志
+            fprintf(log_file, "%s [%s]: 正在尝试初始化数据库。", current_time, LOGLEVEL_INFO);
+            fprintf(log_file, "%s [%s]: 正在初始化考试数据库。", current_time, LOGLEVEL_INFO);
+            char examination_init_command[] = "CREATE TABLE examinations(\n"
+                             "id TEXT PRIMARY KEY   NOT NULL\n"
+                             "name TEXT             NOT NULL\n"
+                             "start_time INT        NOT NULL\n"
+                             "end_time INT          NOT NULL\n"
+                             "allow_answer_when_expired INT NOT NULL\n"
+                             "random_question INT   NOT NULL\n"
+                             ");";
+            get_current_time(current_time, sizeof(current_time));
+            fprintf(
+                log_file, 
+                "%s [%s]: 数据库 %s 的考次表 examinations 初始化成功！%s", 
+                current_time, 
+                LOGLEVEL_INFO, 
+                EXAMINATION_DB, 
+                examination_init_command
+            );
+            char questions_init_command[] = "CREATE TABLE questions(\n)"
+                                            "id TEXT PRIMARY KEY    NOT NULL\n"
+                                            "exam_id TEXT           NOT NULL\n"
+                                            "num1 INTEGER           NOT NULL\n"
+                                            "op INTEGER             NOT NULL\n"
+                                            "num2 INTEGER           NOT NULL\n"
+                                            ");";
+            get_current_time(current_time, sizeof(current_time));
+            fprintf(
+                log_file, 
+                "%s [%s]: 数据库 %s 的问题表 questions 初始化成功！%s", 
+                current_time, 
+                LOGLEVEL_INFO, 
+                EXAMINATION_DB, 
+                questions_init_command
+            );
+            char scores_init_command[] =    "CREATE TABLE scores(\n"
+                                            "id TEXT PRIMARY KEY    NOT NULL\n"
+                                            "exam_id TEXT           NOT NULL\n"
+                                            "user_id TEXT           NOT NULL\n"
+                                            "score INTEGER          NOT NULL\n"
+                                            "expired_flag INTEGER   NOT NULL\n"
+                                            ");";
+            get_current_time(current_time, sizeof(current_time));
+            fprintf(
+                log_file, 
+                "%s [%s]: 数据库 %s 的成绩表 scores 初始化成功！%s", 
+                current_time, 
+                LOGLEVEL_INFO, 
+                SCORES_DB, 
+                scores_init_command
+            );
+            char users_init_command[] = "CREATE TABLE users(\n"
+                                        "id TEXT PRIMARY KEY        NOT NULL\n"
+                                        "username TEXT PRIMARY KEY  NOT NULL\n"
+                                        "hashpass TEXT              NOT NULL\n"
+                                        "salt TEXT                  NOT NULL\n"
+                                        "role INTEGER               NOT NULL\n"
+                                        "name TEXT                  NOT NULL\n"
+                                        "class TEXT                 NOT NULL\n"
+                                        "number INTEGER             NOT NULL\n"
+                                        ");";
+            get_current_time(current_time, sizeof(current_time));
+            fprintf(
+                log_file, 
+                "%s [%s]: 数据库 %s 的用户表 users 初始化成功！%s", 
+                current_time, 
+                LOGLEVEL_INFO, 
+                USER_DB, 
+                users_init_command
+            );
+        }
+        else
+        {
+            // 新建出错了，将出错信息写入日志并退出程序
+            get_current_time(current_time, sizeof(current_time));
+            fprintf(log_file, "%s [%s]: 初始化失败，创建文件夹 '%s' 时遇到问题：错误码 %d，%s\n", current_time, LOGLEVEL_ERROR, DB_FOLDER, errno, strerror(errno));
+            exit(1);
+        }
+    }
+    else
+    {
+        get_current_time(current_time, sizeof(current_time));
+        fprintf(log_file, "%s [%s]: 文件夹 '%s' 已存在。\n", current_time, LOGLEVEL_INFO, DB_FOLDER); // 文件夹已经存在时记录日志
+    }
+        get_current_time(current_time, sizeof(current_time));
+        fprintf(log_file, "%s [%s]: 初始化完成！\n", current_time, LOGLEVEL_INFO); // 文件夹已经存在时记录日志
+    // 关闭日志文件
+    fclose(log_file);}
+
+/**
+ * @brief **仅用于调试**，用于调试初始化整个项目
+ * 
+ * @return int 程序运行结束状态
+ */
+int main()
+{
+    SetConsoleOutputCP(65001);
+    initialize();
+    return 0;
+}
