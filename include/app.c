af414944989a2c2b6f6988d8f15684e893b4dec7
GamerNoTitle <GamerNoTitle@outlook.com>
2024-12-18 23:55:42 +0800

    refactor: defined two functions on building linked list

--- include/app.c
+++ include/app.c
@@ -9,9 +9,14 @@ History:        暂无
         Author: 吴沛熹
         ID: GamerNoTitle
         Modification:   [+] 新建了本文件，并把model.c中有关APP运行的自定义函数挪入本文件
+    1.  Date: 2024/12/18
+        Author: 吴沛熹
+        ID: GamerNoTitle
+        Modification:   [+] 定义了问题列表相关函数 `generate_question_list`、`randomize_question_list`
  */
 
 #include <math.h>
+#include <stdlib.h>
 
 /**************************** 问题模型部分 ****************************/
 /**
@@ -57,3 +62,31 @@ int judge(float result, float user_input)
 }
 
 /**************************** 问题模型部分结束 ****************************/
+
+/**************************** 考试问题生成模块部分开始 ****************************/
+
+/**
+ * @brief 生成问题链表
+ * 
+ * @param exam_id 需要生成问题链表的考试id，会根据此ID从题库获取题目
+ * @param question_list_to_return 需要返回的问题数组
+ * @return int 成功返回 0，否则返回 1
+ */
+int generate_question_list(const char *exam_id, struct Question *question_list_to_return)
+{
+    ;
+}
+
+/**
+ * @brief 将问题链表随机顺序化
+ * 
+ * @param question_list_to_return 返回随机化后问题链表的变量
+ * @param original_question_list 原来的问题链表
+ * @return int 成功返回 0，否则返回 1
+ */
+int randomize_question_list(struct Question *question_list_to_return, struct Question *original_question_list)
+{
+    ;
+}
+
+/**************************** 考试问题生成模块部分结束 ****************************/
